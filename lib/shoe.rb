9e4fb7e5d14da574020f9da25a3d6f8603e33922
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-05-01 14:08:24 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :size, :color
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def brand
+    @brand
+  end
+
+  def material=(material)
+    @material = material
+  end
+
+  def condition
+    @condition
+  end
+end
